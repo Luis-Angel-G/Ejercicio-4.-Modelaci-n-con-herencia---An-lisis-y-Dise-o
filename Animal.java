8eab3bb5207c8cc35ddc35c1cf047da6304b6571
Luis-Angel-G <langelgirona@gmail.com>
2024-10-01 19:38:31 -0600

    Se quitan setters

--- Animal.java
+++ Animal.java
@@ -1,35 +1,23 @@
-class Animal{
-    protected String nombrecientifico;
+class Animal {
+    protected String nombreCientifico;
     protected String habitat;
     protected double esperanza;
 
-    public Animal(String nombrecientifico, String habitat, double esperanza){
-        this.nombrecientifico = nombrecientifico;
+    public Animal(String nombreCientifico, String habitat, double esperanza) {
+        this.nombreCientifico = nombreCientifico;
         this.habitat = habitat;
         this.esperanza = esperanza;
     }
 
     public String getNombreCientifico() {
-        return nombrecientifico;
-    }
-
-    public void setNombreCientifico(String nombrecientifico) {
-        this.nombrecientifico = nombrecientifico;
+        return nombreCientifico;
     }
 
     public String getHabitat() {
         return habitat;
     }
 
-    public void setHabitat(String habitat) {
-        this.habitat = habitat;
-    }
-
     public double getEsperanza() {
         return esperanza;
-    }
-
-    public void setEsperanza(double esperanza) {
-        this.esperanza = esperanza;
     }
 }
