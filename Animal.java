b5efc498ee5429bd247698a108a036b59fbc96d3
Luis-Angel-G <langelgirona@gmail.com>
2024-10-01 19:56:33 -0600

    Se agregan setters

--- Animal.java
+++ Animal.java
@@ -13,11 +13,23 @@ class Animal {
         return nombreCientifico;
     }
 
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
     public String getHabitat() {
         return habitat;
     }
 
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
     public double getEsperanza() {
         return esperanza;
+    }
+
+    public void setEsperanza(double esperanza) {
+        this.esperanza = esperanza;
     }
 }
