efc2e4c1f9fac1cb4fafb4ddf0ea7c56ff0ee5ec
Luis-Angel-G <langelgirona@gmail.com>
2024-10-01 19:09:20 -0600

    Se agregan unos métodos

--- Gestion.java
+++ Gestion.java
@@ -1,5 +1,24 @@
 import java.util.ArrayList;
 
 public class Gestion{
+    Animal animal;
     ArrayList<Animal> listaAnimales = new ArrayList<>();
+
+    public void agregarAnimal(Animal animal){
+        listaAnimales.add(animal);
+    }
+
+    public void eliminarAnimal(Animal animal){
+        listaAnimales.remove(animal);
+    }
+
+    public String mostrarAnimales(){
+        for(int i < listaAnimales.size(); i++){
+            String anima;
+            anima = animal;
+            return anima;
+        }
+    }
+
+    
 }
