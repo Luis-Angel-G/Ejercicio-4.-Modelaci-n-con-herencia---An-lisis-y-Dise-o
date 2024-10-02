7f1b54393540db39ef36a55653358d1f321d4100
Luis-Angel-G <langelgirona@gmail.com>
2024-10-01 19:51:15 -0600

    Se corrige el constructor

--- Primate.java
+++ Primate.java
@@ -3,9 +3,9 @@ class Primate extends Mamifero {
     private int inteligencia;
     private double cerebro;
 
-    public Primate(String nombreCientifico, String habitat, double esperanza, int pelaje, int crias, double peso, 
-                   int gestacion, boolean extincion, int dieta, String especie, int recinto, int tamanio,
-                   int estructura, int inteligencia, double cerebro) {
+    public Primate(String nombreCientifico, String habitat, double esperanza, int pelaje, int crias, 
+                   double peso, int gestacion, boolean extincion, int dieta, String especie, 
+                   int recinto, int tamanio, int estructura, int inteligencia, double cerebro) {
         super(nombreCientifico, habitat, esperanza, pelaje, crias, peso, gestacion, extincion, dieta, especie, recinto, tamanio);
         this.estructura = estructura;
         this.inteligencia = inteligencia;
